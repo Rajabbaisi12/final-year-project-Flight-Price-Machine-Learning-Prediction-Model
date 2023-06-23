128f9ef7ab2ef4b5ee91bb11b01976c0a66e89e3
Rajabbaisi12 <114404139+Rajabbaisi12@users.noreply.github.com>
2023-06-23 12:33:05 +0300

    Update from flask import Flask, request, render.py

--- webhtml/from flask import Flask, request, render.py
+++ webhtml/from flask import Flask, request, render.py
@@ -12,7 +12,7 @@ model = pickle.load(open("flight2_lr.pkl", "rb"))
 @app.route("/")
 @cross_origin()
 def home():
-    return render_template("home.html")
+    return render_template("HOME.html")
 
 @app.route("/predict", methods = ["GET", "POST"])
 @cross_origin()
